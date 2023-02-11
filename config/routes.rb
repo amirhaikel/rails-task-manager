d0b245e7cebcc4517f620dd53ff227d5005cbc94
amir haikel <amir.haikel@gmail.com>
2023-02-11 16:23:00 +0800

    Challenge 2 - Code Added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+   # READ ALL TASKS
+
+   get 'tasks', to: 'tasks#index'
+   # CREATE A TASK
+   get 'tasks/new', to: 'tasks#new', as: 'new_task'
+    # READ ONE TASKS
+   get 'tasks/:id', to: 'tasks#show', as: 'task'
+   # CREATE A TASK
+   post 'tasks', to: 'tasks#create'
+   # UPDATE A TASK
+   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+   patch 'tasks/:id', to: 'tasks#update'
 end
